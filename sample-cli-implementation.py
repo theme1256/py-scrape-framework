558675ee5e1fbdfbe551a7ffa63b275ea6ab9a01
Folkmann <theme1256@gmail.com>
2018-12-28 02:45:27 +0100

    Sample updated to CLI-version and web-version was added

--- sample-cli-implementation.py
+++ sample-cli-implementation.py
@@ -18,25 +18,20 @@ class SmartFormatter(argparse.HelpFormatter):
 parser = argparse.ArgumentParser(description='Script for testing Scraper', formatter_class=SmartFormatter)
 parser.add_argument('-d', '--debug', help='R|Enabels debugging', required=False, action="store_true")
 parser.add_argument('-v', '--verbose', help='R|Increases output, when debug is enabled', required=False, action="store_true")
-parser.add_argument('-e', '--element', help='R|Tells the script if it should find another element than the default "title"', required=False, action="store", type=str, default="title")
-parser.add_argument('-u', '--url', help='R|Tells the script if it should load another url, other than the default: "http://bettermotherfuckingwebsite.com/"', required=False, action="store", type=str, default="http://bettermotherfuckingwebsite.com/")
+parser.add_argument('-e', '--element', help='R|Tells the script if it should find another element than the default "title"', required=True, action="store", type=str, default="title")
+parser.add_argument('-u', '--url', help='R|Tells the script if it should load another url, other than the default: "http://bettermotherfuckingwebsite.com/"', required=True, action="store", type=str, default="http://bettermotherfuckingwebsite.com/")
 args = parser.parse_args()
 
 debug = args.debug
 verbose = args.verbose
-test = args.test
-url = args.testurl
-element = args.testelement
+url = args.url
+element = args.element
 
 
 
 if __name__ == '__main__':
 	Scraper = Scraper(debug, verbose)
 	reply = Scraper.find(url, element)
-
-	# Set JSON header for good measure
-	print("Cache-control: no-cache")
-	print("Content-Type: application/json; charset=UTF-8")
 
 	# Handle data from Scraper
 	if reply is None:
---
+++ sample-web-implementation.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*- 
+from scraper import Scraper
+import pyodbc 
+import cgitb
+import cgi
+import sys
+import json
+
+
+
+if __name__ == '__main__':
+	Scraper = Scraper()
+	reply = Scraper.find(url, element)
+
+	# Fetch parameters from URL
+	form = cgi.FieldStorage()
+	data = form.getvalue("url")
+	element = form.getvalue("element")
+
+	# Set JSON header for good measure
+	print("Cache-control: no-cache")
+	print("Content-Type: application/json; charset=UTF-8")
+
+	# Verify that some input was given
+	if url is "" or url is None:
+		print(json.dumps({"error": True, "reason": "Missing URL", "msg": "No URL where data should be fetched, was given"}))
+	elif element is "" or element is None:
+		print(json.dumps({"error": True, "reason": "Missing element", "msg": "No element to search for was given"}))
+	else:
+		# Handle data from Scraper
+		if reply is None:
+			print(json.dumps({"error": True, "reason": "Communication error", "msg": "RequestException thrown"}))
+		elif hasattr(soup, status_code):
+			print(json.dumps({"error": True, "reason": "{} status returned".format(reply.status_code), "msg": "Server replied with status code {}".format(reply.status_code)}))
+		else:
+			print(json.dumps({"error": False, "reason": "object found", "data": reply}))
